e0ee33d48d2b8bb384a77f44ff02322ae34cab7d
Herrmylan <78021598+Herrmylan@users.noreply.github.com>
2023-10-29 20:57:59 +0100

    H1 add ujra

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <H1>Ez egy project with CsaBA</H1>
         <a
           className="App-link"
           href="https://reactjs.org"
