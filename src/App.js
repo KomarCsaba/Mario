d168cbd33d1bf307210e7fa30de91bbfc996f9ea
Herrmylan <78021598+Herrmylan@users.noreply.github.com>
2023-10-29 21:09:50 +0100

    Proba

--- src/App.js
+++ src/App.js
@@ -4,19 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header>
         <p>
-          Edit <code>src/App.js</code> and save to reload. And fuck you!
+          "Hello world"
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
