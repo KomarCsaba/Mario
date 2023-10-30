7c6076339d45084712c03fe358f6c106a564f31e
KomarCsaba <komar.csaba05@gmail.com>
2023-10-30 23:47:04 +0100

    comment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
 
+/*
 function Square({ value, onSquareClick }) { //létrehoz egy buttont amibe bele van írva a button
   return (
     <button className="square" onClick={onSquareClick}>
@@ -8,15 +9,12 @@ function Square({ value, onSquareClick }) { //létrehoz egy buttont amibe bele v
     </button>
   );
 }
-
-function onClick(){
-  return;
-}
+*/
 
 function App() {
   return (
     <div className="App">
-      <Square value = {"valami"} onSquareClick={addEventListener("click", onClick())}/>
+      
 
       
       
