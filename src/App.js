cd9aabd65b8a40bdc782989d04985baec363cc28
Herrmylan <herr.milan.boldi@gmail.com>
2023-10-31 00:11:01 +0100

    Nemisakaromlátni

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-/*
-function Square({ value, onSquareClick }) { //létrehoz egy buttont amibe bele van írva a button
-  return (
-    <button className="square" onClick={onSquareClick}>
-      {value}
-    </button>
-  );
-}
 
-function onClick() { //ha megnyomod a gombot...
-  console.log("Irj ki valamit");
-  let x = document.createElement("p");
-  let t = document.createTextNode("Paragraph is created.");
-  x.appendChild(t);
-  document.body.appendChild(x);
-}
-*/
+
 
 function App() {
   return (
