dd5915834e7659e866716a31035d7733c23a9de9
Herrmylan <78021598+Herrmylan@users.noreply.github.com>
2023-10-30 21:51:50 +0100

    VALAMI

--- src/App.js
+++ src/App.js
@@ -4,10 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          "Hello world"
+          Edit <code>src/App.js</code> and save to reload. VALAMI
         </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
