eef46cfaebc4dcbaa1d500eb624848965387a02f
KomarCsaba <komar.csaba05@gmail.com>
2023-10-31 00:56:43 +0100

    ClassPlan

--- src/Plan.js
+++ src/Plan.js
@@ -1,15 +1,15 @@
 import './Plan.css';
 import React from 'react';
 
-function squareCreate(width, height) {
-    const elem = document.createElement("div");
-    elem.style.width = width;
-    elem.style.height = height;
-
-    const pg = document.createElement("p");
-    elem.appendChild(pg);
-
-    return elem;
+class Plan{
+    squareCreate(width, height) {
+        const elem = document.createElement("div");
+        elem.style.width = width;
+        elem.style.height = height;
+        const pg = document.createElement("p");
+        elem.appendChild(pg);
+        return elem;
+    }
 }
    
-export default squareCreate;
+export default Plan;
