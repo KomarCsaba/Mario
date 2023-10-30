4f16439bb6615804d474f5c499e95a83e747376b
Herrmylan <herr.milan.boldi@gmail.com>
2023-10-30 22:35:36 +0100

    Kitörölve

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. VALAMI, Még valami
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
