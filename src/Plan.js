5a8215f1a4d70d20b8f5ef1e6e25d683316f3071
KomarCsaba <komar.csaba05@gmail.com>
2023-10-31 00:49:50 +0100

    Plan

--- src/Plan.js
+++ src/Plan.js
@@ -1,14 +1,15 @@
+import './Plan.css';
 
 function squareCreate(width, height) {
+    const elem = document.createElement("div");
+    elem.style.width = width;
+    elem.style.height = height;
 
+    const pg = document.createElement("p");
+    elem.appendChild(pg);
     return (
-        <div  class="square"> {/*  */}
-        <p class="text">Square text</p>
-        </div> 
+        elem
     )
-
-
-
 }
    
 export default squareCreate;
