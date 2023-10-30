ca13fe3615ce87b45a38c8a7b40d148b7f7cc553
Herrmylan <herr.milan.boldi@gmail.com>
2023-10-30 22:59:14 +0100

    G***

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>Főpróba</p>
+        <H1>GECI</H1>
         <a
           className="App-link"
           href="https://reactjs.org"
