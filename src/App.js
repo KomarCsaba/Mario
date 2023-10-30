0fb203a5e3a215b05759483c3ae8152cfa4b56a4
Herrmylan <herr.milan.boldi@gmail.com>
2023-10-30 23:42:59 +0100

    Komment

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-
-function Square({ value, onSquareClick }) {
+/*
+function Square({ value, onSquareClick }) { //létrehoz egy buttont amibe bele van írva a button
   return (
     <button className="square" onClick={onSquareClick}>
       {value}
@@ -10,12 +10,21 @@ function Square({ value, onSquareClick }) {
   );
 }
 
+function onClick() { //ha megnyomod a gombot...
+  console.log("Irj ki valamit");
+  let x = document.createElement("p");
+  let t = document.createTextNode("Paragraph is created.");
+  x.appendChild(t);
+  document.body.appendChild(x);
+}
+*/
+
 function App() {
   return (
     <div className="App">
 
       
-
+      
     </div>
   );
 }
