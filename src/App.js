20998a101e7e18f68ced88b6d1b81c23f6899a7d
KomarCsaba <78021777+KomarCsaba@users.noreply.github.com>
2023-10-30 22:14:36 +0100

    MegValami

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. VALAMI
+          Edit <code>src/App.js</code> and save to reload. VALAMI, Még valami
         </p>
         <a
           className="App-link"
