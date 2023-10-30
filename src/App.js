b95f726f273d96afb9c5301085097d8f2b59a264
KomarCsaba <78021777+KomarCsaba@users.noreply.github.com>
2023-10-30 23:14:06 +0100

    Square

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
 function App() {
   return (
     <div className="App">
