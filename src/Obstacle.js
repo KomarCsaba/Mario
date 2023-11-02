0fb1c7165793eb94b67972db59f20208b564edff
Herrmylan <herr.milan.boldi@gmail.com>
2023-11-02 18:54:56 +0100

    Kommit

--- src/Obstacle.js
+++ src/Obstacle.js
@@ -3,21 +3,58 @@ import React, {Component} from 'react';
 
 class Plan extends Component{
     render() {
-        const {width, height} = this.props;
-        const divStyle = {
-            width: width,
-            height: height
+        const {id} = this.props;
+        const divStyle1 = {
+            width: "50px",
+            height: "50px"
         };
-        return (
-            <div className='container'>   
-                <div className="content">
-                    
+        const divStyle2 = {
+            width: "50px",
+            height: "70px",
+        };
+        const divStyle3 = {
+            width: "150px",
+            height: "50px"
+        }
+
+        if (id == 1) {
+            return (
+                <div className='container'>   
+                    <div className="content">
+                        
+                    </div>
+                    <div style={divStyle1} className="square">
+                        <p>Square1</p>
+                    </div>
                 </div>
-                <div style={divStyle} className="square">
-                    <p>Square</p>
+            );
+        }
+        else if (id == 2) {
+            return (
+                <div className='container'>
+                    <div className='content'>
+
+                    </div>
+                    <div style={divStyle2} className="square">
+                        <p>Square2</p>
+                    </div>
                 </div>
-            </div>
-        );
+            )
+        }
+        else if (id == 3) {
+            return (
+                <div className='container'>
+                    <div className='content'>
+
+                    </div>
+                    <div style={divStyle3} className="floatingSquare">
+                        <p>Sqare3</p>
+                    </div>
+                </div>
+            )
+        }
+
+        
     }
 }
 
