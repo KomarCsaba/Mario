38a3043de6a06d9aa7f6ac6de07642d095e710a9
Herrmylan <herr.milan.boldi@gmail.com>
2023-11-03 20:23:52 +0100

    Kommit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
 import './App.css';
 import Plan from "./Obstacle";
 import Score from "./Score";
+import React, { useEffect, useRef, useState } from "react";
+import { ReactDOM } from 'react';
+
+let random1;
+let random2;
+let random3;
+
 function App() {
+
+  const myElementRef = useRef(null);
+  const [top, setTop] = useState('');
+  const [bottom, setBottom] = useState('');
+  const [left, setLeft] = useState('');
+  const [right, setRight] = useState('');
+  const [width, setWidth] = useState('');
+  const [height, setHeight] = useState('');
+
+  useEffect(() => {
+    const el = myElementRef.current;
+    const rect = el.getBoundingClientRect();
+    setTop(rect.top);
+    setBottom(rect.bottom);
+    setLeft(rect.left);
+    setRight(rect.right);
+    setWidth(rect.width);
+    setHeight(rect.height);
+  }, []);
+
+  
+
   for (let index = 1; index > 0; index++) {
-    let random1 = Math.floor(Math.random() * 3) + 1;
-    let random2 = Math.floor(Math.random() * 3) + 1;
-    let random3 = Math.floor(Math.random() * 3) + 1;
+    random1 = Math.floor(Math.random() * 3) + 1;
+    random2 = Math.floor(Math.random() * 3) + 1;
+    random3 = Math.floor(Math.random() * 3) + 1;
     return ( 
       <div className='component App'>
+
+        <div className="container">
+            <h3>React Js Get Element Position</h3>
+            <div ref={myElementRef}>This is my element</div>
+
+            <pre>Top: {top}</pre>
+            <pre>Right: {right}</pre>
+            <pre>Bottom: {bottom}</pre>
+            <pre>Left: {left}</pre>
+            <pre>Height: {height}</pre>
+            <pre>Width: {width}</pre>
+        </div>
+
         <div className='component'>
           <Plan id={random1}/>
         </div>
@@ -27,5 +69,7 @@ function App() {
     ); 
   }
 }
+
+ReactDOM.render(<App />, document.querySelector(".App"));
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -8,7 +8,13 @@ import './Player.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
   <React.StrictMode>
-    <div className="App">  
+    <div>
+      <div>
+        <h1>Score:</h1>
+        
+      </div>
+    </div>
+  <div className="App">  
       <div className="componentPlayer">
         <Player />
       </div>
@@ -18,3 +24,4 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
     </div>
   </React.StrictMode>
 );
+
