f3fce89ec165ac5e88ddbe9e38107322d838160f
KomarCsaba <komar.csaba05@gmail.com>
2023-11-11 23:44:12 +0100

    MovingComponent

--- src/MovingComponent.js
+++ src/MovingComponent.js
@@ -1,10 +1,28 @@
-import React, { Component } from 'react';
-import './MovingComponent.css';
+import React, { useState, useEffect } from 'react';
+import './MovingComponent.css'; // Create a CSS file for styling
 
-class MovingComponent extends Component {
-  render() {
-    return <div className="component">Your moving content</div>;
-  }
-}
+const MovingComponent = () => {
+  const [components, setComponents] = useState([]);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setComponents(prevComponents => {
+        // Add a new component to the right
+        const newComponent = <div key={Date.now()} className="moving-component" />;
+        return [...prevComponents, newComponent];
+      });
+    }, 200); // Adjust the interval based on your preference
+
+    return () => clearInterval(intervalId); // Cleanup on component unmount
+  }, []);
+
+  return (
+    <div className="moving-container">
+      {components.map((component, index) => (
+        <React.Fragment key={index}>{component}</React.Fragment>
+      ))}
+    </div>
+  );
+};
 
 export default MovingComponent;
--- src/index.js
+++ src/index.js
@@ -1,33 +1,14 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Player from './Player';
 import './Player.css';
-import ScrollingContainer from './ScrollingContainer.js';
 import React, { useState, useEffect } from 'react';
+import MovingComponent from './MovingComponent.js';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
   <React.StrictMode>
-    <div className="page">
-      <div className="content">
-      <h1>Score:</h1>
-      </div>
-      <div className="bottom-section">
-        <div className="fixed-component">
-          <Player/>
-        </div>
-        <div className="scroll-container">
-          <ScrollingContainer/>
-        </div>
-        <div className="scroll-container">
-          <ScrollingContainer/>
-        </div>
-        <div className="scroll-container">
-          <ScrollingContainer/>
-        </div>
-      </div>
-    </div>
+    <MovingComponent/>
   </React.StrictMode>
 );
 
