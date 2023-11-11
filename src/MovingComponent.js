1987117595cef01dbe34d772c60d1114aa30f78d
KomarCsaba <komar.csaba05@gmail.com>
2023-11-11 23:49:54 +0100

    Színek

--- src/MovingComponent.js
+++ src/MovingComponent.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './MovingComponent.css'; // Create a CSS file for styling
+import './MovingComponent.css'; // Import the CSS file for styling
 
 const MovingComponent = () => {
   const [components, setComponents] = useState([]);
@@ -7,8 +7,12 @@ const MovingComponent = () => {
   useEffect(() => {
     const intervalId = setInterval(() => {
       setComponents(prevComponents => {
-        // Add a new component to the right
-        const newComponent = <div key={Date.now()} className="moving-component" />;
+        // Generate a random color for each component
+        const randomColor = `#${Math.floor(Math.random()*16777215).toString(16)}`;
+        
+        // Add a new component to the right with a random background color
+        const newComponent = <div key={Date.now()} className="moving-component" style={{ backgroundColor: randomColor }} />;
+        
         return [...prevComponents, newComponent];
       });
     }, 200); // Adjust the interval based on your preference
