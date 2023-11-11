dbe728f294c08c2920feb70cfad20812ca74f53d
KomarCsaba <komar.csaba05@gmail.com>
2023-11-12 00:18:52 +0100

    Moving Component updated

--- src/MovingComponent.js
+++ src/MovingComponent.js
@@ -7,12 +7,8 @@ const MovingComponent = () => {
   useEffect(() => {
     const intervalId = setInterval(() => {
       setComponents(prevComponents => {
-        // Generate a random color for each component
         const randomColor = `#${Math.floor(Math.random()*16777215).toString(16)}`;
-        
-        // Add a new component to the right with a random background color
         const newComponent = <div key={Date.now()} className="moving-component" style={{ backgroundColor: randomColor }} />;
-        
         return [...prevComponents, newComponent];
       });
     }, 200); // Adjust the interval based on your preference
