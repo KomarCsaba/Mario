c304183e03a294280b9141835a6324b9df4bf279
KomarCsaba <komar.csaba05@gmail.com>
2023-11-13 20:31:05 +0100

    Clean up

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import ReactDOM from 'react-dom/client';
 import React from 'react';
-import Player from './Player.js';
 import FinalObstacle from './FinalObstacle.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
   <React.StrictMode>
-    {/* <Player/> */}
     <FinalObstacle/>
   </React.StrictMode>
 );
