50b92223b43f32aa0a870dcbf8735c14eb91b254
Herrmylan <herr.milan.boldi@gmail.com>
2023-11-19 21:28:04 +0100

    scorehatter2

--- src/End.js
+++ src/End.js
@@ -2,16 +2,19 @@ import "./Obstacle.js";
 
 const EndDiv = ({score, highScore, onRestart}) => {
 
+  const hatter = `url(${require('./assets/scorehatter.jpg')})`;
+
     const divStyle = {
       position: "absolute",
       top: 0,
       left: 0,
       width: "100%",
       height: "100%",
-      backgroundColor: "blue",
       zIndex: "9999",
       textAlign: "center",
       fontFamily: "Inconsolata",
+      backgroundImage: hatter,
+      backgroundSize: "100% 100%"
     }
   
     console.log("lefutottam");
